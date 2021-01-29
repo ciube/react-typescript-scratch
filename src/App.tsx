17b154a16f217dc71b1cab69a17b072318c819fd
Matteo Langone <mlangone@sogei.it>
2021-01-29 15:58:41 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export function App() {
+    return (
+        <>
+            <h1>Project Name</h1>
+            <p>This is going to be great</p>
+        </>
+    )
+}
